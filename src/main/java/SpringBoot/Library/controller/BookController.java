09d2cc2849fdbb13564f0fefc4b4bd50308e6682
Concepticus <carlos_qq_silva@hotmail.com>
2024-06-10 15:37:55 +0100

    changed the request handle

--- src/main/java/SpringBoot/Library/controller/BookController.java
+++ src/main/java/SpringBoot/Library/controller/BookController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/books")
+@RequestMapping("/api/books")
 public class BookController {
 
     private final BookService bookService;
