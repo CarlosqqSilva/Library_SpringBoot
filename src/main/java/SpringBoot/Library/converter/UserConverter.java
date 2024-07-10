27ad08b9771a59502a9711679440f2c3873d2490
Concepticus <carlos_qq_silva@hotmail.com>
2024-07-10 16:01:36 +0100

    added PUT controller/service + dto and converter into the user

--- src/main/java/SpringBoot/Library/converter/UserConverter.java
+++ src/main/java/SpringBoot/Library/converter/UserConverter.java
@@ -2,6 +2,7 @@ package SpringBoot.Library.converter;
 
 import SpringBoot.Library.dto.user.UserGetDto;
 import SpringBoot.Library.dto.user.UserPostDto;
+import SpringBoot.Library.dto.user.UserPutDto;
 import SpringBoot.Library.model.User;
 
 public class UserConverter {
@@ -23,6 +24,14 @@ public class UserConverter {
                 user.getEmail());
     }
 
+    public static UserPutDto fromModelToUserPutDto(User user) {
+        return new UserPutDto(
+                user.getUserName(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getEmail());
+    }
+
     public static User fromUserDtoToModel(UserPostDto userPostDto) {
         return User.builder()
                 .userName(userPostDto.userName())
@@ -35,6 +44,16 @@ public class UserConverter {
     public static User fromUserGetDtoToModel(UserGetDto userGetDto) {
         return User.builder()
                 .userName(userGetDto.userName())
+                .build();
+    }
+
+    public static User fromUserPutDtoToModel(Long id, UserPutDto userPutDto) {
+        return User.builder()
+                .id(id)
+                .userName(userPutDto.userName())
+                .firstName(userPutDto.firstName())
+                .lastName(userPutDto.lastName())
+                .email(userPutDto.email())
                 .build();
     }
 }
